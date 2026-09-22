26a84e1c7f1642a61666794338a12693a793c27e
SAIFA-SARUT <saifa.sarut@gmail.com>
2026-09-22 15:34:42 +0700

    Update config.js
    เชื่อมต่อ Supabase และปิดโหมดสาธิต

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const APP_CONFIG = {
   supabaseUrl: 'https://kxnohxbipjtzdaoorauh.supabase.co',
   supabaseAnonKey: 'sb_publishable__hOBakAjyIPWEeczvcQZOQ_6p8XRwuT',
-  demoMode: true,
+  demoMode: false,
   provinceName: 'เชียงราย',
   mapCenter: [19.91, 99.84],
   mapZoom: 9
